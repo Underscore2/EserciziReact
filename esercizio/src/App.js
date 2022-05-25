00a85656b4413d9089d8e2afd02b9f7ef43e4b70
Gabriel <Gabrielteddelavoro@gmail.com>
2022-05-25 20:33:05 +0200

    Feat/Components 03 - Done

---
+++ esercizio/src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Helloworld from "./Hello";
+
+class App extends React.Component() {
+
+    render() {
+
+        return (
+            <div>
+                <Helloworld />
+            </div>
+        )
+    }
+}
+
+export default App
