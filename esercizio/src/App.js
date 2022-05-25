d546c5e001f141b7db70bb96c6ead1b393cec7b9
Gabriel <Gabrielteddelavoro@gmail.com>
2022-05-25 21:00:19 +0200

    feat/Props 04 - Done

--- esercizio/src/App.js
+++ esercizio/src/App.js
@@ -1,12 +1,13 @@
 import React from "react";
 import Welcome from "./Welcome";
+const TaggoStrongo = <strong>Giorginho</strong>
 class App extends React.Component {
 
     render() {
 
         return (
             <div>
-                <Welcome name="Jhonny" age="1" />
+                <Welcome name={TaggoStrongo} age="1" />
             </div>
         )
     }
