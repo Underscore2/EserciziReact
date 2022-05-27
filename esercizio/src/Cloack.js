8e667e4ac4e98447a942407cbf2e799ce400dafe
Gabriel <Gabrielteddelavoro@gmail.com>
2022-05-27 19:45:59 +0200

    State 03 - Done

--- esercizio/src/Cloack.js
+++ esercizio/src/Cloack.js
@@ -3,16 +3,16 @@ import React from "react";
 class Cloack extends React.Component {
 
     state = {
-        cloack: 0
+        cloack: this.props.cloack
     }
     constructor(props) {
         super(props)
 
         setInterval(() => {
             this.setState((state) => {
-                return { cloack: state.cloack + 1 }
+                return { cloack: state.cloack + this.props.count }
             })
-        }, 1000)
+        }, this.props.interval)
     }
 
 
@@ -20,6 +20,12 @@ class Cloack extends React.Component {
         return <h1>Cloack: {this.state.cloack}</h1>
     }
 
+}
+
+Cloack.defaultProps = {
+    interval: 1000,
+    count: 1,
+    cloack: 1
 }
 
 export default Cloack;
