bed304b60973e5e2e6c9484d97295133282b0fb9
Gabriel lavoro <Gabrielteddelavoro@gmail.com>
2022-06-11 10:53:34 +0200

    List 04 - Done

--- esercizio/src/TodoList.js
+++ esercizio/src/TodoList.js
@@ -7,21 +7,36 @@ class TodoList extends React.Component {
         todos: ['Cane', 'Gatto', 'Topo', 'Sara'],
         value: ''
     }
-    resetList = ()=>{ this.setState({todos:['Cane', 'Gatto', 'Topo', 'Sara']})}
+
+    resetList = () => { this.setState({ todos: ['Cane', 'Gatto', 'Topo', 'Sara'] }) }
     inputSaver = (event) => this.setState({ value: event.target.value });
     dataPusher = () => {
         this.setState((state) => {
-            
+
             return {
                 todos: state.todos.concat(this.state.value),
-                value: state.value=''
-            
-          
+                value: state.value = ''
+
+
             }
         })
     }
 
+  /*   removeItem = (index) => {
+        this.setState((state) => {
+            const array = state.todos.splice(index,1)
+            return{todos:state.todos}
+   
+        })
+    } */
 
+    removeItem = (todo) => {
+        this.setState((state) => {
+            const array = state.todos.filter((item)=>{return item !== todo})
+            return {todos: state.todo= array}
+   
+        })
+    }
     render() {
         return (
             <>
@@ -29,11 +44,15 @@ class TodoList extends React.Component {
                     <h1>To do list</h1>
                     {this.state.todos.map((todo, index) => {
                         return (
-                            <li key={index}>{todo}</li>
+                            <div>
+                                <li name='li' key={index}>{todo}</li> <button key={index+'btn'} onClick={()=>{this.removeItem(todo)}}>Remove</button>
+                            </div>
+
+
                         )
                     })}
-                    <input type="text" name='inputin' value={this.state.value}onChange={this.inputSaver}></input>
-                    <button  onClick={this.dataPusher}>
+                    <input type="text" name='inputin' value={this.state.value} onChange={this.inputSaver}></input>
+                    <button onClick={this.dataPusher}>
                         Add
                     </button>
                     <button onClick={this.resetList}> Reset </button>
