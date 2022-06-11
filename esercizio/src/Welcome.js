87b7e607cabf5963e5b1f7a42eef626ece711a03
Gabriel lavoro <Gabrielteddelavoro@gmail.com>
2022-06-11 11:14:53 +0200

    Style 01 - Done

---
+++ esercizio/src/Welcome.js
@@ -0,0 +1,13 @@
+import react from 'react';
+
+class Welcome extends react.Component{
+    render(){
+        return(
+
+            <h1 className='welcome'>
+                Welcome
+            </h1>
+        )
+    }
+}
+export default Welcome;
