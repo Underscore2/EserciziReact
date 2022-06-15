d5ff5e0cad77e6cc6e2d7eed2892ff15e478d945
Gabriel lavoro <Gabrielteddelavoro@gmail.com>
2022-06-15 10:20:20 +0200

    use effect 02 - Done

--- esercizio/src/App.js
+++ esercizio/src/App.js
@@ -1,15 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
 import Counter from './Counter';
-
+import { useState } from 'react';
 function App() {
+
+  const [toggle,setToggle]=useState(true)
+
+
+
   function onCounterChange(counter){
     console.log(`The state now is ${counter}`)
   }
 
+  function handleButton(){
+    setToggle(s=>!s)
+  }
+
   return (
     <div className="App">
-     <Counter onCounterChange={onCounterChange}/>
+     {toggle && <Counter onCounterChange={onCounterChange}/>}
+     <button onClick={handleButton} >UAAAAAAAAAa</button>
      
     </div>
   );
--- esercizio/src/Counter.js
+++ esercizio/src/Counter.js
@@ -1,28 +1,36 @@
-import {useState,useEffect} from 'react'
+import { useState, useEffect } from 'react'
 
 
-function Counter ({initialValue=0,onCounterChange}){
-    
-    const [counter,setCount]=useState(initialValue)
+function Counter({ initialValue = 0 }) {
+
+
+    const [counter, setCount] = useState(initialValue)
+    const intervalID = ()=> setInterval(()=>{
+              setCount(counter + 1)
+          },1000)
+
+    useEffect(() => {
+
+        intervalID()
+        
+    }, [counter])
+
     useEffect(()=>{
-    onCounterChange(counter)}    
-    ,[counter])
-    
-    
-    function handlerButton(){
-        
-        return (setCount(counter+1))
-        
-    }
-    
-return(
-    
-    
-    <>
-    <h1>Counter: {counter}</h1>
-    <button onClick={handlerButton}>Click me!</button>
-    </>
-)
+        return()=>{
+            clearInterval(intervalID)
+            console.log('Verrò smontato, ma due volte perché sono in strict mode. LOL!')
+        }
+    },[])
+
+
+
+    return (
+
+
+        <>
+            <h1>Counter: {counter}</h1>
+        </>
+    )
 
 }
 export default Counter;
