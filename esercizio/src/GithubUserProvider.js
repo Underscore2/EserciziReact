9139fe0bb39fd8bb75034b0d8df6ea32cf6bac01
Gabriel lavoro <Gabrielteddelavoro@gmail.com>
2022-06-16 16:28:30 +0200

    Use effect 03 FIX - Done

--- esercizio/src/GithubUserProvider.js
+++ esercizio/src/GithubUserProvider.js
@@ -1,5 +1,5 @@
 import { useEffect,useState } from "react"
-
+  
 export default function GithubUserProvider({username}){
    const [data,setData]=useState('')
     const [error,setError]=useState(false)
