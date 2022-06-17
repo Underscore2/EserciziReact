d762cd7175ef9210a099bcfedad45055ba58de1f
Gabriel lavoro <Gabrielteddelavoro@gmail.com>
2022-06-17 16:34:59 +0200

    use Ref 01 - Done

---
+++ esercizio/src/CarDetails.js
@@ -0,0 +1,13 @@
+import {useRef} from 'react'
+export default function CarDetails({info}) {
+    const targetModel= useRef()
+    const targetColor= useRef()
+    const targetYear= useRef()
+    return (
+        <>
+        <input ref={targetModel} onChange={()=>{targetModel.current.value=''}} type='text' defaultValue={info.model}></input>
+        <input ref={targetYear}  onChange={()=>{targetYear.current.value=''}} type='number' defaultValue={info.year}></input>
+        <input ref={targetColor}  onChange={()=>{targetColor.current.value=''}} type='text' defaultValue={info.color}></input>
+        </>
+    )
+}
