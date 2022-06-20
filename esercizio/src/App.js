b735f563e31c9af3006a794e81052346777c7aba
Gabriel <Gabrielteddelavoro@gmail.com>
2022-06-20 10:28:26 +0200

    Router 04 - Done

--- esercizio/src/App.js
+++ esercizio/src/App.js
@@ -1,20 +1,30 @@
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, Link } from 'react-router-dom';
 import './App.css';
 import Welcome from './Welcome';
 import Counter from './Counter';
 import ShowGithubUser from './ShowGithubUser';
 function App() {
+  const margin = {
+    margin: "5px"
+  }
 
   return (
     <div className="App">
-      <Routes>
-        <Route path='/' element={<Welcome name='Giangiorgio' />}> </Route>
-        <Route path='/counter' element={<Counter />}></Route>
-        <Route path='/githubuser'>
-          <Route path=':name' element={<ShowGithubUser />} />
-        </Route>
-      </Routes>
+      <>
+        <div>
+          <Link style={margin} to="/counter" > Counter</Link>
+          <Link style={margin} to="/githubuser/Underscore2" >Link2</Link>
+          <Link style={margin} to="/" > Welcome</Link>
 
+        </div>
+        <Routes>
+          <Route path='/' element={<Welcome name='Giangiorgio' />}> </Route>
+          <Route path='/counter' element={<Counter />}></Route>
+          <Route path='/githubuser'>
+            <Route path=':name' element={<ShowGithubUser />} />
+          </Route>
+        </Routes>
+      </>
 
     </div>
   );
