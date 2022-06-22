ca7bcac943ff15fd19bcb1e517b9e0aac54f72b0
Gabriel lavoro <Gabrielteddelavoro@gmail.com>
2022-06-22 19:35:47 +0200

    SWR 01 - Done

--- esercizio/src/GithubUserProvider.js
+++ esercizio/src/GithubUserProvider.js
@@ -1,15 +1,13 @@
 import useFetch from "./useFetch"
 
-export default function GithubUserProvider({username}){
-const {data,error}=useFetch(username)
-   
-   
-    return(
-        <>
-         <h1>{data.login}</h1>
-         <img src={data.avatar_url}></img>
-        <h1>{data.name}</h1>
-        {error && <h1>{error.message} </h1>}
-        </>
-    )
+export default function GithubUserPrivder({username}){
+    const {data,error,isLoading} = useFetch(username)
+    return <>
+    {isLoading && <h1>Loading..</h1>}
+    {error && <h1>{error.message}</h1>}
+    {data && <div><h1>{data.login}</h1>
+    <img src={data.avatar_url}></img></div>}
+
+
+    </>
 }
--- esercizio/src/useFetch.js
+++ esercizio/src/useFetch.js
@@ -1,31 +1,6 @@
-import { useState,useEffect } from "react"
-
+import useSWR from "swr";
 export default function useFetch(username){
-
-    const [data,setData]=useState(username)
-    const [error,setError]=useState(false)
-  
-    async function userProvider (){ 
-    try {
-        const response = await fetch(`https://api.github.com/users/${username}`);
-        if(response.status === 200){
-      const pack = await response.json()
-      setData(pack)
-      console.log(pack)
-    }
-      else{
-          throw new Error('Impossibile comunicare con il server.');
-        }
-    }catch(errore){
-        return setError(errore);
-    }
-    
-  }
-   useEffect(()=>{
-userProvider()
-
-   },[])
-
-   return{data,error}
-
+    const fetcher=url=>fetch(url).then((resp)=>resp.json());
+   const{data,error}= useSWR(`https://api.github.com/users/${username}`,fetcher)
+    return{data,error, isLoading: !data && !error};
 }
