4e878135d3f274bf9cf7155c071c924bec51ded4
Gabriel <Gabrielteddelavoro@gmail.com>
2022-06-27 17:24:03 +0200

    REDUX 02 - Done

--- esercizio/src/state/Store.js
+++ esercizio/src/state/Store.js
@@ -1,3 +1,7 @@
 import { createStore } from "@reduxjs/toolkit"
 import { counterReducer } from "./CounterState"
 export const store = createStore(counterReducer)
+
+store.subscribe(() => {
+    console.log(store.getState())
+})
